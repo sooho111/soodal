8ce61883382703aac0fc458af8d0d54740ba97a0
DESKTOP-4D4I9RS\mignon030 <dlalsdud08@naver.com>
2022-02-01 12:56:30 +0900

    dal

--- SoodalProject/src/soodal/Main.java
+++ SoodalProject/src/soodal/Main.java
@@ -8,10 +8,19 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		int score = sc.nextInt();
 		
-		
+		if(score <= 100 && score >= 90) {
+			System.out.println("A");
+		} else if(score >= 80 && score < 90) {
+			System.out.println("B");			
+		} else if(score >=70 && score < 80) {
+			System.out.println("C");			
+		} else if(score >=60 && score < 70) {
+			System.out.println("D");			
+		} else {
+			System.out.println("F");			
+		}
 	}
 
 }
