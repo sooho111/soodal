d08526017447fd2a54dba16d9ca039e29d80a84f
DESKTOP-C9BCU3O\dlals <dlalsdud08@naver.com>
2022-02-05 22:42:58 +0900

    dal

--- SoodalProject/src/soodal/Solution.java
+++ SoodalProject/src/soodal/Solution.java
@@ -2,18 +2,31 @@ package soodal;
 
 public class Solution {
 	
-	public int[] solution(int[] lottos, int[] win_nums) {
-		int[] answer = {};
-		int[] lotto = new int[45];
-		
-		for(int i=0; i < lotto.length; i++) {        	
-			lotto[i] = (int)(Math.random()*45 +1);
-		}
-		for(int i=0; i < lotto.length; i++) {
-			System.out.println("?" + lotto[i]);   
-		}
-		
-		return answer;
+	public int[] solution(int[] lottos, int[] win_nums) { // 프로그래머스 로또 최고, 최저 순위 구하기
+		int zero = 0;
+        int matched = 0;
+        
+        for (int l : lottos) {
+            if (l == 0) { zero++;
+            System.out.println("zero === " + zero); }
+            else {
+                for (int w : win_nums) {
+                    if (l == w) {
+                        matched++;
+                        System.out.println("matched === " + matched);
+                        break;
+                    }
+                }
+            }
+        }
+        
+        int min = matched;
+        int max = matched + zero;
+        
+        System.out.println("min === " + min);
+        System.out.println("max === " + max);
+        
+        int[] answer = {Math.min(7 - max, 6), Math.min(7 - min, 6)};
+        return answer;
 	}
-
 }
